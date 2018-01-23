35bd058f6d1377d7dd467f94d185d5190ec157a0
zhaiweifeng <zhaiweifeng@localhost.localdomain>
2018-01-23 02:30:04 -0800

    modify assert

--- assertion/aes128_assert.sv
+++ assertion/aes128_assert.sv
@@ -7,20 +7,14 @@ interface aes128_assert();
    logic rst_n;
 
    wire assert_load = top.dut.load_i;
-   wire assert_encrypt_decrypt = top.dut.decrypt_i;
+   wire assert_ready = top.dut.ready_o;
 
-   property load_high_encrypt_decrypt_low_at_same_time;
+   property cycle_check;
       @(posedge clk) disable iff(!rst_n)
-      (assert_load == 1) |-> (assert_encrypt_decrypt == 0);
+      (assert_load == 1) |-> ##505 (assert_ready == 1);
    endproperty
 
-   property load_high_encrypt_decrypt_high_at_same_time;
-      @(posedge clk) disable iff(!rst_n)
-      (assert_load == 1) |-> (assert_encrypt_decrypt == 1);
-   endproperty
-
-   cover_load_high_encrypt_decrypt_low_at_same_time : cover property (load_high_encrypt_decrypt_low_at_same_time);
-   cover_load_high_encrypt_decrypt_high_at_same_time : cover property (load_high_encrypt_decrypt_high_at_same_time);
+   assert_cycle_check : assert property(cycle_check);
 
 endinterface
 
